fb22a553340e511e115dabb929ccd8096a87b4b7
DarkLuciferKing <72114930+DarkLuciferKing@users.noreply.github.com>
2021-04-02 22:05:16 -0400

    Add files via upload

---
+++ scripts/player.js
@@ -0,0 +1,59 @@
+    class Player {
+        constructor(){
+            this.name = "";
+            this.distance = 0
+            this.index = 0
+            this.rank = 0
+        }
+
+        getPlayerCount(){
+            var playerCountRef = database.ref("playerCount")
+            playerCountRef.on("value",function(data){
+                console.log(data);
+                playerCount = data.val()
+            }, function(errorMsg) {
+                console.log(errorMsg)
+            });
+        }
+
+        updatePlayerCount(countVal){
+            database.ref("/").update({
+                playerCount : countVal
+            });            
+        }
+
+        updatePlayerDetails() {
+            var playerRef = database.ref("players/player" + this.index);
+            playerRef.set({
+                playerName : this.name,
+                distance : this.distance
+            });
+        }
+
+        static readPlayers(){
+            var playersRead = database.ref("players");
+            playersRead.on("value", function(data){
+                allPlayers = data.val()
+            }, function(errorMsg) {
+                console.log(errorMsg)
+            });
+        }
+
+        static getRank(){
+            var playerRankRef = database.ref("Rank");
+            playerRankRef.on("value", function(data){
+                playerRank = data.val()
+            }, function(errorMsg) {
+                console.log(errorMsg)
+            });
+         }
+
+         updateRank(rankVal){
+             database.ref("/").update({
+                Rank : rankVal
+             });
+
+         }
+         
+
+    }
